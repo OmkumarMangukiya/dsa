273f4e156f233be6eedd64ca6cc6f689783720b2
OmkumarMangukiya <ommangukiya8+github@gmail.com>
2025-09-07 09:43:21 +0530

    POTD

--- QuestionOfTheDay/07092025.cpp
+++ QuestionOfTheDay/07092025.cpp
@@ -12,7 +12,7 @@ public:
         }
         else{
             int cnt = -(n/2);
-            for(int i =0;i<n;i++){
+            for(int i =0;i<=n;i++){
                 if(cnt!=0)
                 ret.push_back(cnt);
                 cnt++;
