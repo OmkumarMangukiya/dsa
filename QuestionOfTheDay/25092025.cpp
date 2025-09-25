ad43c39691a11ede7514f91b2b6fccbbc44fc158
OmkumarMangukiya <ommangukiya8+github@gmail.com>
2025-09-25 07:46:08 +0530

    POTD

---
+++ QuestionOfTheDay/25092025.cpp
@@ -0,0 +1,13 @@
+https://leetcode.com/problems/triangle/?envType=daily-question&envId=2025-09-25
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int n = triangle.size();
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = 0; j < triangle[i].size(); j++) {
+                triangle[i][j] += min(triangle[i+1][j], triangle[i+1][j+1]);
+            }
+        }
+        return triangle[0][0];
+    }
+};
