d222c6e0ea6ddea988e5b8fcf8087c3d8f660bc8
OmkumarMangukiya <ommangukiya8+github@gmail.com>
2025-09-26 06:35:47 +0530

    POTD

---
+++ QuestionOfTheDay/26092025.cpp
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/valid-triangle-number/?envType=daily-question&envId=2025-09-26
+class Solution {
+public:
+    int triangleNumber(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        int count = 0;
+
+        for (int i = n - 1; i >= 2; i--) {
+            int left = 0, right = i - 1;
+            while (left < right) {
+                if (nums[left] + nums[right] > nums[i]) {
+                    count += right - left;
+                    right--;
+                } else {
+                    left++;
+                }
+            }
+        }
+        return count;
+    }
+};
