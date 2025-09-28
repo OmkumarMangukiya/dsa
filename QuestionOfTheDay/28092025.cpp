9f0a9b4b0dfd034d58fc417348d1a67b6045dd6a
OmkumarMangukiya <ommangukiya8+github@gmail.com>
2025-09-28 07:09:35 +0530

    POTD

---
+++ QuestionOfTheDay/28092025.cpp
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/largest-perimeter-triangle/?envType=daily-question&envId=2025-09-28
+class Solution {
+public:
+    int largestPerimeter(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        reverse(nums.begin(),nums.end());
+        int n = nums.size();
+        for (int i = 0; i < n - 2; i++) {
+            if (nums[i] < nums[i+1] + nums[i+2]) {
+                return nums[i] + nums[i+1] + nums[i+2];
+            }
+        }
+        return 0;
+    }
+};
